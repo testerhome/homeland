0078b1cdc80cc0df697147541d49aaa09a32f585
lanzhiheng <lanzhihengrj@gmail.com>
2020-10-02 22:50:33 +0800

    hotfix: Fix plugin missing issue.

--- app/overrides/controllers/homeland/jobs/jobs_controller_override.rb
+++ app/overrides/controllers/homeland/jobs/jobs_controller_override.rb
@@ -1,7 +1,10 @@
-Homeland::Jobs::JobsController.class_eval do
-  private
+# 规避插件没有安装所导致的抱错
+if defined?(Homeland::Jobs)
+  Homeland::Jobs::JobsController.class_eval do
+    private
 
-  def set_node
-    @node = Node.find(Node.job_id)
+    def set_node
+      @node = Node.find(Node.job_id)
+    end
   end
 end
