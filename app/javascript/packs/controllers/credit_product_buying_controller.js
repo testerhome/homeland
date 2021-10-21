6a98ccd693f88e0448578cabe627a3b78252719b
atpking <atpking@gmail.com>
2021-10-22 00:55:25 +0800

    add product variant

--- app/javascript/packs/controllers/credit_product_buying_controller.js
+++ app/javascript/packs/controllers/credit_product_buying_controller.js
@@ -1,17 +1,58 @@
 import { Controller } from "stimulus"
+const SelectClassList = ["tw-text-blue-400", "tw-border-2", 'tw-border-solid', "tw-border-blue-400"]
 
 export default class extends Controller {
   static values = {
-    variants: Array,
-    current_varint: Number
+    currentVariantId: String,
+    currentUserCredit: Number
   }
+
+
   static targets = [
-    "main_image",
-    "price",
-    "current_variant_input"
+    "variants",
+    "creditPrice",
+    "variantImage",
+    "buyButton"
   ]
 
+
+  selectVariant(event){
+    event.preventDefault();
+    let ele = event.target
+    if (ele.classList.contains("variant")){
+      this._selectElement(ele)
+    }
+
+
+  }
+
   connect(){
-    console.log(this.variantsValue)
+    console.log(this.currentVarintIdValue)
+    if (this.currentVariantIdValue != "") {
+      let ele = [].slice.call(this.variantsTarget.children).find(ele => ele.dataset.id == this.currentVariantIdValue)
+      this._selectElement(ele)
+    }
+  }
+
+  _resetVariantSelect(){
+    [].slice.call(this.variantsTarget.children).forEach(ele => {
+      SelectClassList.forEach(className => {ele.classList.remove(className)})
+    })
+
+  }
+  _selectElement(ele){
+    this._resetVariantSelect()
+    SelectClassList.forEach(className => {ele.classList.add(className)})
+    // ele.classList.add("tw-text-blue-400")
+    this.creditPriceTarget.innerText = ele.dataset.price
+    this.currentPrice = parseInt(ele.dataset.price)
+    this.variantImageTarget.src = ele.dataset.image
+    if (this.currentUserCreditValue < this.currentPrice){
+      this.buyButtonTarget.classList.add("disabled")
+      this.buyButtonTarget.innerText = '没有足够的积分'
+    }else {
+      this.buyButtonTarget.classList.remove("disabled")
+      this.buyButtonTarget.innerText = '购买'
+    }
   }
 }
